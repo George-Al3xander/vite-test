c03daacb9442140b3f9d07fb6100a48e1ac75308
George <george.valuiskyi@gmail.com>
2023-05-16 15:09:38 +0300

    first commit

---
+++ src/Test.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+
+
+
+
+const Test = () => {
+
+  return(
+    <div><h1>Hello2</h1>
+      <button>Hello2</button>
+    </div>
+  )
+}
+
+export default Test
